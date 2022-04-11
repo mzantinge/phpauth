6066fd20ccd2c815dde3dd8bd371dd4a32940ea9
mzantinge <103180149+mzantinge@users.noreply.github.com>
2022-04-11 08:24:07 +0200

    Update index.php

--- back/index.php
+++ back/index.php
@@ -1,4 +1,11 @@
 <?php
+
+      foreach(getallheaders() as$name=> $value) {
+        if($name== 'X-MS-TOKEN-AAD-ACCESS-TOKEN') {
+          $access_token=$value;
+        }
+        echo"<br><b>$name: </b>$value";
+
 header('Content-type: application/json');
 echo '{ "server": "' . gethostname() .'" }'; 
 ?>
